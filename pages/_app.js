84938476ee4e581cecde271f42dbeffd02cc3a03
Samuel Kori <samuel@wearequantum.co.uk>
2023-10-17 10:04:53 +0100

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-PTZMFQ50Y5"/>
+      <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-PTZMFQ50Y5', {
+          page_path: window.location.pathname,
+          });
+          `,
+          }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
